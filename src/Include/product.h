f1850ee3daa96a4fe52ab3e2fabd9affe48d358d
Steven F <stev.frer@gmail.com>
2022-11-01 16:42:47 +0100

    Add product.h - Hw version - Fw version (based on date)

---
+++ src/Include/product.h
@@ -0,0 +1,21 @@
+/*!
+* \file product.h
+* \brief This file contains all product characteristics
+*           such as HW version or FW version
+*/
+
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#define HW_VERSION = 0x01u
+
+/*  Fw version
+*   Format: YY.MM.DD.R 
+*   YY = Year
+*   MM = Month
+*   DD = Day
+*   R = Revision
+*/
+#define FW_VERSION = Ox7E6B01u  /* 2022.11.01.1 */
+
+#endif /* PRODUCT_H */
